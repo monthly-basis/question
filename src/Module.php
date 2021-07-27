80288464a0ebc5453f20fa600efac668904b2ef8
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-27 04:59:42 +0000

    Inject missing Config entity

--- src/Module.php
+++ src/Module.php
@@ -406,6 +406,7 @@ class Module
                 },
                 QuestionService\Question\Questions\Similar::class => function ($sm) {
                     return new QuestionService\Question\Questions\Similar(
+                        $sm->get(QuestionEntity\Config::class),
                         $sm->get(QuestionFactory\Question::class),
                         $sm->get(QuestionTable\Question::class),
                         $sm->get(QuestionTable\QuestionSearchMessage::class)
