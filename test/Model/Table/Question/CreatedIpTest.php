166a125620f0733208469e051da961f08d8fe5e4
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-12-31 06:47:05 +0000

    Write method to select count where recently deleted by user for reason

---
+++ test/Model/Table/Question/CreatedIpTest.php
@@ -0,0 +1,126 @@
+<?php
+namespace MonthlyBasis\QuestionTest\Model\Table\Question;
+
+use DateTime;
+use MonthlyBasis\Question\Model\Table as QuestionTable;
+use MonthlyBasis\Memcached\Model\Service as MemcachedService;
+use MonthlyBasis\LaminasTest\TableTestCase;
+
+class CreatedIpTest extends TableTestCase
+{
+    protected function setUp(): void
+    {
+        $this->dropAndCreateTable('question');
+
+        $this->questionTable = new QuestionTable\Question(
+            $this->getAdapter()
+        );
+        $this->createdIpTable = new QuestionTable\Question\CreatedIp(
+            $this->getAdapter(),
+        );
+    }
+
+    public function test_selectCountWhereCreatedIpDeletedDateTimeGreaterThanDeletedUserIdDeletedReason()
+    {
+        $result = $this->createdIpTable->selectCountWhereCreatedIpDeletedDateTimeGreaterThanDeletedUserIdDeletedReason(
+            '1.2.3.4',
+            (new DateTime())->modify('-5 minutes'),
+            0,
+            'foul language',
+        );
+        $this->assertSame(
+            [
+                'COUNT(*)' => '0',
+            ],
+            $result->current(),
+        );
+
+        $this->questionTable->insertDeleted(
+            null,
+            'subject',
+            'message',
+            'name',
+            '1.2.3.4',
+            0,
+            'foul language',
+        );
+        $result = $this->createdIpTable->selectCountWhereCreatedIpDeletedDateTimeGreaterThanDeletedUserIdDeletedReason(
+            '1.2.3.4',
+            (new DateTime())->modify('-5 minutes'),
+            0,
+            'foul language',
+        );
+        $this->assertSame(
+            [
+                'COUNT(*)' => '1',
+            ],
+            $result->current(),
+        );
+
+        $this->questionTable->insertDeleted(
+            null,
+            'subject',
+            'message',
+            'name',
+            '255.255.255.255',
+            32,
+            'a different reason',
+        );
+        $result = $this->createdIpTable->selectCountWhereCreatedIpDeletedDateTimeGreaterThanDeletedUserIdDeletedReason(
+            '1.2.3.4',
+            (new DateTime())->modify('-5 minutes'),
+            0,
+            'foul language',
+        );
+        $this->assertSame(
+            [
+                'COUNT(*)' => '1',
+            ],
+            $result->current(),
+        );
+
+        $this->questionTable->insertDeleted(
+            null,
+            'subject',
+            'message',
+            'name',
+            '1.2.3.4',
+            0,
+            'a different reason',
+        );
+        $result = $this->createdIpTable->selectCountWhereCreatedIpDeletedDateTimeGreaterThanDeletedUserIdDeletedReason(
+            '1.2.3.4',
+            (new DateTime())->modify('-5 minutes'),
+            0,
+            'foul language',
+        );
+        $this->assertSame(
+            [
+                'COUNT(*)' => '1',
+            ],
+            $result->current(),
+        );
+
+        $this->questionTable->insertDeleted(
+            null,
+            'subject 2',
+            'message 2',
+            'name',
+            '1.2.3.4',
+            0,
+            'foul language',
+        );
+        $result = $this->createdIpTable->selectCountWhereCreatedIpDeletedDateTimeGreaterThanDeletedUserIdDeletedReason(
+            '1.2.3.4',
+            (new DateTime())->modify('-5 minutes'),
+            0,
+            'foul language',
+        );
+        $this->assertSame(
+            [
+                'COUNT(*)' => '2',
+            ],
+            $result->current(),
+        );
+    }
+}
