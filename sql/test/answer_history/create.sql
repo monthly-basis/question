5db1bfc51868e9bfbfa7adf2145fbc4e1f1ca354
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-03-25 05:11:41 +0000

    Add `modified_user_id` column to `answer_history` table

--- sql/test/answer_history/create.sql
+++ sql/test/answer_history/create.sql
@@ -3,6 +3,7 @@ CREATE TABLE `answer_history` (
     `answer_id` int(10) unsigned not null,
     `name` varchar(255) default null,
     `message` text,
+    `modified_user_id` int unsigned DEFAULT NULL,
     `modified_reason` varchar(255) DEFAULT NULL,
     `created` datetime not null,
     PRIMARY KEY (`answer_history_id`),
