975293dcb06c0fb1bf0285efd12495da552ae13b
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-03-27 16:14:13 +0000

    Catch exception when conditionally inserting

--- src/Model/Service/Question/QuestionViewNotBotLog/ConditionallyInsert.php
+++ src/Model/Service/Question/QuestionViewNotBotLog/ConditionallyInsert.php
@@ -1,6 +1,7 @@
 <?php
 namespace MonthlyBasis\Question\Model\Service\Question\QuestionViewNotBotLog;
 
+use Laminas\Db\Adapter\Exception\InvalidQueryException;
 use Laminas\Db\TableGateway\TableGateway;
 use MonthlyBasis\Question\Model\Entity as QuestionEntity;
 use MonthlyBasis\Superglobal\Model\Service as SuperglobalService;
@@ -22,11 +23,16 @@ class ConditionallyInsert
             return false;
         }
 
-        $this->questionViewNotBotLogTableGateway
-            ->insert([
-                'question_id' => $questionEntity->getQuestionId(),
-                'ip' => $_SERVER['REMOTE_ADDR'],
-            ]);
+        try {
+            $this->questionViewNotBotLogTableGateway
+                ->insert([
+                    'question_id' => $questionEntity->getQuestionId(),
+                    'ip' => $_SERVER['REMOTE_ADDR'],
+                ]);
+        } catch (InvalidQueryException $invalidQueryException) {
+            return false;
+        }
+
         return true;
     }
 }
--- test/Model/Service/Question/QuestionViewNotBotLog/ConditionallyInsertTest.php
+++ test/Model/Service/Question/QuestionViewNotBotLog/ConditionallyInsertTest.php
@@ -1,8 +1,8 @@
 <?php
 namespace MonthlyBasis\QuestionTest\Model\Service\Question\QuestionViewNotBotLog;
 
-use Laminas\Db\TableGateway\TableGateway;
 use Laminas\Db\Adapter\Exception\InvalidQueryException;
+use Laminas\Db\TableGateway\TableGateway;
 use MonthlyBasis\Question\Model\Entity as QuestionEntity;
 use MonthlyBasis\Question\Model\Service as QuestionService;
 use MonthlyBasis\Superglobal\Model\Service as SuperglobalService;
@@ -44,7 +44,33 @@ class ConditionallyInsertTest extends TestCase
         $this->assertFalse($result);
     }
 
-    public function test_conditionallyInsert_isNotBot_true()
+    public function test_conditionallyInsert_invalidQueryExceptionThrown_false()
+    {
+        $questionEntity = (new QuestionEntity\Question())
+            ->setQuestionId(12345)
+            ;
+        $this->botServiceMock
+            ->expects($this->once())
+            ->method('isBot')
+            ->willReturn(false)
+            ;
+        $this->questionViewNotBotLogTableGatewayMock
+            ->expects($this->once())
+            ->method('insert')
+            ->with([
+                'question_id' => 12345,
+                'ip' => '1.2.3.4',
+            ])
+            ->will($this->throwException(new InvalidQueryException()))
+            ;
+
+        $result = $this->conditionallyInsertService->conditionallyInsert(
+            $questionEntity
+        );
+        $this->assertFalse($result);
+    }
+
+    public function test_conditionallyInsert_isNotBotAndNoExceptionThrown_true()
     {
         $questionEntity = (new QuestionEntity\Question())
             ->setQuestionId(12345)
@@ -62,6 +88,7 @@ class ConditionallyInsertTest extends TestCase
                 'ip' => '1.2.3.4',
             ])
             ;
+
         $result = $this->conditionallyInsertService->conditionallyInsert(
             $questionEntity
         );
