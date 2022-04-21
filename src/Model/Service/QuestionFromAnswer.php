920e95f731488bd420320c35a0afe1dc606ac495
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-04-21 14:02:28 +0000

    Remove unnecessary comments

--- src/Model/Service/QuestionFromAnswer.php
+++ src/Model/Service/QuestionFromAnswer.php
@@ -6,22 +6,12 @@ use MonthlyBasis\Question\Model\Factory as QuestionFactory;
 
 class QuestionFromAnswer
 {
-    /**
-     * Construct.
-     *
-     * @param QuestionFactory\Question $questionFactory
-     */
     public function __construct(
         QuestionFactory\Question $questionFactory
     ) {
         $this->questionFactory = $questionFactory;
     }
 
-    /**
-     * Get question from answer.
-     *
-     * @return QuestionEntity\Question
-     */
     public function getQuestionFromAnswer(
         QuestionEntity\Answer $answerEntity
     ): QuestionEntity\Question {
