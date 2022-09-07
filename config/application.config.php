87d0c9165684b791bd3d0c85f3ca262ffa143d0d
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-09-07 00:27:17 +0000

    Add MonthlyBasis\Vote to list of modules

--- config/application.config.php
+++ config/application.config.php
@@ -12,6 +12,7 @@ return [
         'MonthlyBasis\String',
         'MonthlyBasis\Superglobal',
         'MonthlyBasis\User',
+        'MonthlyBasis\Vote',
     ],
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
