1e822f5504cd00ac7d1c949b7839b1868b4345b1
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-05-11 19:06:32 +0000

    Remove template_map

--- src/Module.php
+++ src/Module.php
@@ -36,12 +36,6 @@ class Module
                 'doctype'                  => 'HTML5',
                 'not_found_template'       => 'error/404',
                 'exception_template'       => 'error/index',
-                'template_map' => [
-                    'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-                    'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-                    'error/404'               => __DIR__ . '/../view/error/404.phtml',
-                    'error/index'             => __DIR__ . '/../view/error/index.phtml',
-                ],
                 'template_path_stack' => [
                     'monthly-basis/question' => __DIR__ . '/../view',
                 ],
