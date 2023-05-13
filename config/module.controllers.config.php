2a2c01c2f81bd702224636b1f3c094aa9f649a67
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-05-13 03:05:41 +0000

    Write post action

--- config/module.controllers.config.php
+++ config/module.controllers.config.php
@@ -1,6 +1,10 @@
 <?php
 
+use MonthlyBasis\Flash\Model\Service as FlashService;
 use MonthlyBasis\Question\Controller as QuestionController;
+use MonthlyBasis\Question\Model\Factory as QuestionFactory;
+use MonthlyBasis\Question\Model\Service as QuestionService;
+use MonthlyBasis\Question\Model\Table as QuestionTable;
 
 return [
     'factories' => [
@@ -11,7 +15,16 @@ return [
             return new QuestionController\Questions();
         },
         QuestionController\Questions\Ask::class => function ($sm) {
-            return new QuestionController\Questions\Ask();
+            return new QuestionController\Questions\Ask(
+                $sm->get('config')['monthly-basis']['open-ai'],
+                $sm->get(FlashService\Flash::class),
+                $sm->get(QuestionFactory\Question::class),
+                $sm->get(QuestionService\Question\IncrementAnswerCountCached::class),
+                $sm->get(QuestionService\Question\Insert\Visitor::class),
+                $sm->get(QuestionService\Question\Duplicate::class),
+                $sm->get(QuestionService\Question\Url::class),
+                $sm->get(QuestionTable\Answer::class),
+            );
         },
     ],
 ];
--- src/Controller/Questions/Ask.php
+++ src/Controller/Questions/Ask.php
@@ -2,14 +2,105 @@
 namespace MonthlyBasis\Question\Controller\Questions;
 
 use Laminas\Mvc\Controller\AbstractActionController;
+use MonthlyBasis\Flash\Model\Service as FlashService;
+use MonthlyBasis\Question\Controller as QuestionController;
+use MonthlyBasis\Question\Model\Factory as QuestionFactory;
+use MonthlyBasis\Question\Model\Service as QuestionService;
+use MonthlyBasis\Question\Model\Table as QuestionTable;
 
 class Ask extends AbstractActionController
 {
-    public function __construct()
-    {
+    public function __construct(
+        protected array $openAiConfig,
+        protected FlashService\Flash $flashService,
+        protected QuestionFactory\Question $questionFactory,
+        protected QuestionService\Question\IncrementAnswerCountCached $incrementAnswerCountCachedService,
+        protected QuestionService\Question\Insert\Visitor $visitorInsertQuestionService,
+        protected QuestionService\Question\Duplicate $duplicateService,
+        protected QuestionService\Question\Url $urlService,
+        protected QuestionTable\Answer $answerTable,
+    ) {
     }
 
     public function askAction()
     {
+        if (!empty($_POST)) {
+            return $this->postAction();
+        }
+    }
+
+    protected function postAction()
+    {
+        $errors = [];
+
+        if (empty($_POST['message'])
+            || empty(trim($_POST['message']))
+        ) {
+            $_POST['message'] = '';
+            $errors[]         = 'You did not enter a question.';
+        }
+
+        if ($errors) {
+            $this->flashService->set('message', $_POST['message']);
+            $this->flashService->set('errors', $errors);
+            return $this->redirect()->toRoute('questions/ask')->setStatusCode(303);
+        }
+
+        try {
+            $duplicateQuestion = $this->duplicateService->getDuplicate($_POST['message']);
+            $this->flashService->set(
+                'messageHtml',
+                'This question has already been asked, see below.'
+            );
+            $url = $this->urlService->getUrl($duplicateQuestion);
+            return $this->redirect()->toUrl($url)->setStatusCode(303);
+        } catch (Exception $exception) {
+            // Do nothing.
+        }
+
+        /*
+         * No errors.
+         */
+
+        $this->flashService->set(
+            'messageHtml',
+            'Question successfully posted! <a href="/questions/ask" style="color: blue; white-space: nowrap;">Ask another question.</a>'
+        );
+
+        $questionEntity = $this->visitorInsertQuestionService->insert();
+
+        $message = $this->getMessage($questionEntity);
+        $message = trim($message);
+        if (!empty($message)) {
+            $this->answerTable->insert(
+                values: [
+                    'question_id' => $questionEntity->getQuestionId(),
+                    'message'     => $message,
+                    'imported'    => 1,
+                ]
+            );
+            $this->incrementAnswerCountCachedService->incrementAnswerCountCached($questionEntity);
+        }
+
+        $url = $this->urlService->getUrl($questionEntity);
+        return $this->redirect()->toUrl($url)->setStatusCode(303);
+    }
+
+    protected function getMessage(QuestionEntity\Question $questionEntity): string
+    {
+        ini_set('max_execution_time', 180);
+        $open_ai = new OpenAi($this->openAiConfig['key']);
+        $open_ai->setTimeout(170);
+
+        $completeJson = $open_ai->chat([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ["role" => "user", "content" => $questionEntity->getMessage()]
+            ],
+            'temperature' => 1,
+        ]);
+        $completeArray = json_decode($completeJson, true);
+
+        return $completeArray['choices'][0]['message']['content'] ?? '';
     }
 }
