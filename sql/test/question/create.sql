9fafed57f9707cfeea8b91d477e936a5426120ad
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-07-17 08:27:53 +0000

    Add `views_one_hour_etc` index

--- sql/test/question/create.sql
+++ sql/test/question/create.sql
@@ -48,5 +48,6 @@ CREATE TABLE `question` (
     KEY `deleted_datetime_views_not_bot_one_day` (`deleted_datetime`,`views_not_bot_one_day`),
     KEY `deleted_datetime_views_not_bot_one_hour` (`deleted_datetime`,`views_not_bot_one_hour`),
     KEY `deleted_user_id_deleted_datetime` (`deleted_user_id`, `deleted_datetime`),
-    KEY `message_255` (`message`(255))
+    KEY `message_255` (`message`(255)),
+    KEY `views_one_hour_etc` (`views_not_bot_one_hour` DESC, `views_not_bot_one_day` DESC, `views_not_bot_one_week` DESC, `views_not_bot_one_month` DESC)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
