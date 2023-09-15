befb27342cc03f480b2c7da2a49b010d4cd4dba7
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-09-15 00:45:29 +0000

    Fix typecasts

--- src/Model/Service/Questions/User/Count.php
+++ src/Model/Service/Questions/User/Count.php
@@ -11,7 +11,7 @@ class Count
     ) {
     }
 
-    public function getCount(UserEntity $userEntity): int
+    public function getCount(UserEntity\User $userEntity): int
     {
         $result = $this->userIdTable->selectCountWhereUserId(
             $userEntity->userId
