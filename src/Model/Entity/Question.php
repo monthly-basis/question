25dd10783768e16aa7afdd30f9180f32ced46624
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-11-14 22:28:52 +0000

    Add imageRru properties

--- src/Model/Entity/Question.php
+++ src/Model/Entity/Question.php
@@ -19,6 +19,10 @@ class Question extends QuestionEntity\Post
     protected string $headline;
     protected $history;
     public string $imageRru;
+    public string $imageRru128x128;
+    public string $imageRru256x256;
+    public string $imageRru512x512;
+    public string $imageRru1024x1024;
     protected $message;
     protected DateTime $modifiedDateTime;
     protected string $modifiedReason;
