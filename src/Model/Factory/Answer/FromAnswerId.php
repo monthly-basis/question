429b96973d3ab25ef17910ed14140d565b392a97
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-11-27 19:52:35 +0000

    Clean up code

--- src/Model/Factory/Answer/FromAnswerId.php
+++ src/Model/Factory/Answer/FromAnswerId.php
@@ -1,23 +1,20 @@
 <?php
 namespace MonthlyBasis\Question\Model\Factory\Answer;
 
-use MonthlyBasis\Question\Model\Factory as QuestionFactory;
 use MonthlyBasis\Question\Model\Entity as QuestionEntity;
+use MonthlyBasis\Question\Model\Factory as QuestionFactory;
 use MonthlyBasis\Question\Model\Table as QuestionTable;
 
 class FromAnswerId
 {
     public function __construct(
-        QuestionFactory\Answer $answerFactory,
-        QuestionTable\Answer $answerTable
+        protected QuestionFactory\Answer $answerFactory,
+        protected QuestionTable\Answer $answerTable
     ) {
-        $this->answerFactory = $answerFactory;
-        $this->answerTable   = $answerTable;
     }
 
-    public function buildFromAnswerId(
-        int $answerId
-    ): QuestionEntity\Answer {
+    public function buildFromAnswerId(int $answerId): QuestionEntity\Answer
+    {
         return $this->answerFactory->buildFromArray(
             $this->answerTable->selectWhereAnswerId($answerId)
         );
